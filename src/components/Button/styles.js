f97c120fa103a1ce8ea03499e4560291e628cd12
Thomas Hinders <thomas.hinders@hotmail.com>
2024-01-17 21:20:30 -0300

    produtofinal

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+
+    padding: 20px;
+    border: 1px solid white;
+    background-color: #DCFFB7;
+    color: black;
+    font-size: 24px;
+    flex: 1;
+
+    &:hover{
+        opacity: .6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+   
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #FFBB64;
+        border: 0;
+        padding: 0 10px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        color: white;
+
+        font-size: 24px;
+    }
+`
